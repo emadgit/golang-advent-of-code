fc74a0057b0528458f192de20644d5634d87b9be
Emad Dehnavi <dehnavi@gmx.com>
2025-08-04 10:52:58 +0200

    part-two: increasedGroupSum

--- Golang/adventofcode/Challange-02/main.go
+++ Golang/adventofcode/Challange-02/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -17,13 +18,51 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	groupCount := 0
+	groupSum := 0
+
+	var storedInput []int
+	var groupSumInSlice []int
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(line)
+		num, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println(err)
+		}
+		storedInput = append(storedInput, num)
 	}
+
+	for outerIndex, _ := range storedInput {
+		for _, num := range storedInput[outerIndex : outerIndex+3] {
+			groupSum = groupSum + num
+			groupCount = groupCount + 1
+
+			if groupCount == 3 {
+				groupSumInSlice = append(groupSumInSlice, groupSum)
+				groupCount = 0
+				groupSum = 0
+			}
+		}
+	}
+
+	prevGroupSum := 0
+	increasedGroupSum := 0
+
+	for _, sumForEachGroup := range groupSumInSlice {
+		if (prevGroupSum != 0) && (sumForEachGroup > prevGroupSum) {
+			increasedGroupSum++
+		}
+
+		prevGroupSum = sumForEachGroup
+	}
+
+	fmt.Println("increasedGroupSum: ", increasedGroupSum)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
 }
+
+// Solution for the second part https://adventofcode.com/2021/day/1
