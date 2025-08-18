fbee8b2046be4258e361874b3663ee887dbf3f72
emadgit <dehnavi@gmx.com>
2025-08-18 11:03:24 +0200

    preparing the input

--- Golang/adventofcode/Day4-Part1/main.go
+++ Golang/adventofcode/Day4-Part1/main.go
@@ -3,16 +3,33 @@ package main
 import (
 	"fmt"
 	"learn-go-with-adventofcode/helpers"
+	"strings"
 )
 
 func main() {
 	storedInput := helpers.GetInput("../data/day4-data.txt")
 
-	drawNumbers := []int{17, 11, 37, 7, 89, 48, 99, 28, 56, 55, 57, 27, 83, 59, 53, 72, 6, 87, 33, 82, 13, 23, 35, 40, 71, 47, 78, 2, 39, 4, 51, 1, 67, 31, 79, 69, 15, 73, 80, 22, 92, 95, 91, 43, 26, 97, 36, 34, 12, 96, 86, 52, 66, 94, 61, 76, 64, 77, 85, 98, 42, 68, 84, 63, 60, 30, 65, 19, 54, 58, 24, 20, 25, 75, 93, 16, 18, 44, 14, 88, 45, 10, 9, 3, 70, 74, 81, 90, 46, 38, 21, 49, 29, 50, 0, 5, 8, 32, 62, 41}
+	drawNumbers := []int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1}
 	fmt.Println(drawNumbers)
-	for _, stringValue := range storedInput {
-		// I'll be back tomorrow to solve you :)
+	boards := make([][]string, 3)
+	boards[0] = make([]string, 25)
+	boards[1] = make([]string, 25)
+	boards[2] = make([]string, 25)
 
-		fmt.Println(stringValue)
+	fields := []string{}
+
+	for _, stringValue := range storedInput {
+		fields = strings.Fields(stringValue)
 	}
+
+	counter := 0
+
+	for i := 0; i < 3; i++ {
+		boards[i] = fields[counter : counter+25]
+		counter += 25
+	}
+
+	fmt.Println(boards)
+	// OKAY! So far we have the boards in right format, now I need to solve it, I CAN DO IT!
+
 }
