4115da813c43787663e4ede4dc09ef33c7a70de2
emadgit <dehnavi@gmx.com>
2025-09-04 15:10:14 +0200

    remove k-th node of linked-list problem

--- Nodejs/Linked-List/Remove-k-th-node.js
+++ Nodejs/Linked-List/Remove-k-th-node.js
@@ -14,3 +14,61 @@ Output: 1 -> 2 -> 4 -> 5 -> 7 -> 8 -> 10
 Explanation: After removing every 3rd node of the linked list, the resultant linked list will be: 1 -> 2 -> 4 -> 5 -> 7 -> 8 -> 10.
 
 */
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null
+    };
+}
+
+const inputMaker = () => {
+    let head = new Node(1);
+    head.next = new Node(2);
+    head.next.next = new Node(3);
+    head.next.next.next = new Node(4);
+    head.next.next.next.next = new Node(5);
+    head.next.next.next.next.next = new Node(6);
+
+    return head;
+}
+
+const printList = (curr) => {
+    let output = "";
+    while (curr !== null) {
+        output += curr.data + " ";
+        curr = curr.next;
+    }
+
+    console.log(output.trim());
+}
+
+const solution = (k) => {
+    let head = inputMaker();
+
+    if (head === null || k<= 0) {
+        return head;
+    }
+
+    let currentNode = head;
+    let prev = null;
+    let count = 0;
+
+    while (currentNode != null) {
+        count ++;
+
+        if (count % k === 0) {
+            if (prev !== null) {
+                prev.next = currentNode.next;
+            } else {
+                head = currentNode.next;
+            }
+        } else {
+            prev = currentNode
+        }
+        currentNode = currentNode.next;
+    }
+    printList(head)
+}
+
+solution(2);
